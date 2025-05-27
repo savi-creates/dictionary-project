951c674bfe103a21192506094c4d997a1d94e32e
savi-creates <savannahandresson@gmail.com>
2025-05-27 12:08:39 +0100

    Created new component and added a form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Savannah Andresson</footer>
+      </div>
     </div>
   );
 }
