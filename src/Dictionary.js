0785a808754d6ebc4f3e69476163e4c133de6e9c
savi-creates <savannahandresson@gmail.com>
2025-05-27 12:29:31 +0100

    Added dicitionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`);
+
+    let apiKey = "ta4d13o783b04c3ee4a956ed2febde0f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
