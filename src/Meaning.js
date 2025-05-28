768e04d6ba8fbc50713070fc9e60e015d53b8409
savi-creates <savannahandresson@gmail.com>
2025-05-28 11:17:57 +0100

    Created new component to render synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   const meaning = props.meaning;
@@ -6,12 +7,16 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{meaning.partOfSpeech}</h3>
+      <p>
+        <strong>Definition:</strong>
+      </p>
       {meaning.definition && <p>{meaning.definition}</p>}
       {meaning.example && (
         <p>
           <strong>Example:</strong> "{meaning.example}"
         </p>
       )}
+      <Synonyms synonyms={meaning.synonyms} />
     </div>
   );
 }
